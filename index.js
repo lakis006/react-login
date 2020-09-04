83f996bc1421a9761f7704f1675245e50c9f0aba
Jsquad <lakis006@umn.edu>
2020-09-03 20:41:37 -0500

    added the connections to express and mongoose as well as hid connection password

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors= require("cors");
+require("dotenv").config();
+
+// set up express 
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
+
+// set up mongoose 
