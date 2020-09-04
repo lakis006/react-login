04ee7bb47b175630edb536e17cb3a3a3105eb16f
Jsquad <lakis006@umn.edu>
2020-09-03 20:49:27 -0500

    finished index.js to have connection with mongo on Atlas and express

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
 
 // set up mongoose 
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) throw err;
+    console.log("MongoDB connection established")
+});
