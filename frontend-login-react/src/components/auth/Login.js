7c141292672a13bd41bea73e4dda1ae4b8fb31c9
Jsquad <lakis006@umn.edu>
2020-09-04 22:06:06 -0500

    finished all the authentication error messages for the login portion, which would be 5 characters, a password or password confirmation, and would be updated for the register page & login page contents

--- frontend-login-react/src/components/auth/Login.js
+++ frontend-login-react/src/components/auth/Login.js
@@ -2,16 +2,20 @@ import React, { useState, useContext } from 'react';
 import { useHistory } from "react-router-dom";
 import UserContext from "../../context/UserContext";
 import Axios from "axios";
+import ErrorNotice from '../../misc/ErrorNotice';
 
-function Login() { //at 20:12 of video 6
+function Login() { //
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
+    const [error, setError] = useState();
 
     const { setUserData } = useContext(UserContext);
     const history = useHistory();
 
     const submit = async (e) => {
         e.preventDefault();
+
+        try {
         const loginUser = { email, password };
         const loginRes = await Axios.post(
             "http://localhost:5000/users/login", {
@@ -26,12 +30,19 @@ function Login() { //at 20:12 of video 6
 
         localStorage.setItem("auth-token", loginRes.data.token);
         history.push("/");
+    } catch (err) {
+        err.response.data.msg && setError(err.response.data.msg);
+    }
     };
 
 
     return (
         <div className="page">
         <h2>Login</h2>
+        {error && <ErrorNotice message={error} clearError={
+                () => setError(undefined)}
+                />
+                }
         <form className="form" onSubmit={submit}>
             <label htmlFor="login-email">Email</label>
             <input
