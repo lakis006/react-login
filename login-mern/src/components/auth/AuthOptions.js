cae013d16ca44bc280f6d195442131161b9f2157
Jsquad <lakis006@umn.edu>
2020-09-07 11:03:32 -0500

    had to do my own npx create react app and inserted the source folder. tested the Methods on postman before moving on to the react frontend

---
+++ login-mern/src/components/auth/AuthOptions.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+import { useHistory } from "react-router-dom";
+import UserContext from "../../context/UserContext";
+
+export default function AuthOptions() {
+const {userData, setUserData} = useContext(UserContext); // get user data and set user data for me in separate variables
+
+    const history = useHistory();
+    //register and history are working like a Link works. it changes the current url
+    const register = () => history.push("/register")
+    const login = () => history.push("/login")
+    const logout = () => {
+        setUserData ({
+            token: undefined,
+            user: undefined
+        });
+        localStorage.setItem("auth-token", "");
+    };
+
+    return (
+        <nav className="auth-options">
+            {userData.user ? (
+                <button onClick={logout}>Log Out</button>
+            ) : (
+                <>
+                <button onClick= {register}>Register</button>
+                <button onClick={login}>Log in</button>
+                </>
+        )}
+        </nav>
+    );
+
+}
