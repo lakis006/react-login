4d133c661ee0324a1a18d394ffd98974ad52acc1
Simdog <oguny006@umn.edu>
2020-09-08 17:06:55 -0500

    new update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors= require("cors");
+const cors = require("cors");
 require("dotenv").config();
 
 // set up express 
@@ -15,8 +15,8 @@ app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
 
 // set up mongoose 
 
-mongoose.connect( process.env.MONGODB_URI || process.env.MONGODB_CONNECTION_STRING, {
-    useNewUrlParser: true, 
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGODB_CONNECTION_STRING, {
+    useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
 }, (err) => {
@@ -30,6 +30,8 @@ app.use("/users", require("./routes/userRouter"));
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("frontend-login-react/build"));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'login-mern', 'build', 'index.html'));
 
-}
+    }
 
